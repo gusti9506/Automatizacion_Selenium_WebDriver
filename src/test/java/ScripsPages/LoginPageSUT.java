2a14fc637185723aaec79dce0474201528e53737
Gustavo Adolfo Lopez <95432809+gusti9506@users.noreply.github.com>
2023-02-10 16:10:28 -0300

    Update LoginPageSUT.java

--- src/test/java/ScripsPages/LoginPageSUT.java
+++ src/test/java/ScripsPages/LoginPageSUT.java
@@ -7,59 +7,49 @@ import org.openqa.selenium.support.PageFactory;
 
 //Pagina para hacer el login de ususario
 public class LoginPageSUT {
-	@FindBy(id="email")    //Se localiza el campo email.
+	@FindBy(id="email")
 	WebElement txtEmail;   
 	
-	@FindBy(id="passwd")    //Se localiza el campo password.
+	@FindBy(id="passwd")
 	WebElement txtPass;
 	
-	@FindBy(name="SubmitLogin")   //Se localiza el btn SignIn.
+	@FindBy(name="SubmitLogin")
 	WebElement clickedSignIn;
 	
-	@FindBy(xpath="/html[1]/body[1]/div[1]/div[1]/header[1]/div[2]/div[1]/div[1]/nav[1]/div[2]") //Buttom Sign Out.
+	@FindBy(xpath="/html[1]/body[1]/div[1]/div[1]/header[1]/div[2]/div[1]/div[1]/nav[1]/div[2]")
 	WebElement btnSignOut;
 	
-	@FindBy(xpath="//p[contains(text(),'There is 1 error')]")  //Mensaje error, email invalid.
+	@FindBy(xpath="//p[contains(text(),'There is 1 error')]")
 	WebElement mesageError;
 	
-	@FindBy(xpath="/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/a[1]/i[1]]")  //My Account, con email valid.
+	@FindBy(xpath="/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/a[1]/i[1]]")
 	WebElement myAccount;
 
-	//Se crea el constructor, con mismo nombre de la clase.
 	public LoginPageSUT(WebDriver driver) {
-		//"initElements" revisa e inicializa los elementos (los WebElements).
- 		//el parametro "this", hace que los elementos inicien en "esta" clase en el navegador.
+
 		PageFactory.initElements(driver, this);
-		
 	}
 	
-	//Acciones sobre los elementos (los 3 campos).
-	//Se crea un metodo para cada elemento, y se agrega un String como parametro.
-	//El parametro queda generico, para asi completar con los datos en el @Test (TestHomeAndLogin).
 	public void completarMail(String mail) {
-		txtEmail.sendKeys(mail);             //Representa el ingreso del mail.
+		txtEmail.sendKeys(mail);
 	}
 	
 	public void completarPass(String pass) {
-		txtPass.sendKeys(pass);              //Representa el ingreso del password
+		txtPass.sendKeys(pass);
 	}
 	
 	public void hacerClick() {
-		clickedSignIn.click();               //Representa el click.
+		clickedSignIn.click();
 	}
 	
-	public void clickSignOut() {             //Representa el boton Sign Out (deslogueo).
+	public void clickSignOut() {
 		btnSignOut.click();
 	}
 	
-	public String thereIsOneError() {       //Representa mensaje de error: pass, email invalidos.
+	public String thereIsOneError() {
 		return mesageError.getText();
 	}
 	
-	/*public Boolean myAccountValid() { 	//Representa la imagen de cuenta valida.
-		return myAccount.isDisplayed();
-	}*/
-	//en este metodo incluye las acciones juntas, mismo resultado que los anteriores.
 	public void enterEmailAndPass(String email, String pass) {
 		txtEmail.sendKeys(email);
 		txtPass.sendKeys(pass);
