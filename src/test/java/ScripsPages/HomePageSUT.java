02ca8d72f1d4454c8508003c0a0e19da91e75f97
Gustavo Adolfo Lopez <95432809+gusti9506@users.noreply.github.com>
2023-02-10 16:08:43 -0300

    Update HomePageSUT.java

--- src/test/java/ScripsPages/HomePageSUT.java
+++ src/test/java/ScripsPages/HomePageSUT.java
@@ -8,46 +8,42 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePageSUT {
 	
 	//Identificacion del boton "Sign In".
-	//Importante: con cada elemento identificado se debe hacer una "accion".	
 	@FindBy(linkText="Sign in")
-	WebElement linkSignIn;            //Representacion del hipervinculo Sign In (btn).
+	WebElement linkSignIn;
 	
-	@FindBy(id="email_create")        //Campo email para creacion de cuenta.
+	@FindBy(id="email_create")
 	WebElement emailCreateAccount;
 	
 	@FindBy(name="SubmitCreate")      //Boton Create An Account
 	WebElement btnSubmitCreate;
 	
-	//Este FindBy se agrego despues para hacer otro @Test
 	@FindBy(id="search_query_top")    //Localizador del elemento.
-	WebElement txtSearch;             //Representacion de la barra del buscador.
+	WebElement txtSearch;
 	
 	@FindBy(name="submit_search")      //Localiza la lupa de busqueda
-	WebElement clickedLens;            //y hace click.
+	WebElement clickedLens;
 	
-	//Aca se define el constructor de la pagina (con el mismo nombre de la clase).
+
 	public HomePageSUT (WebDriver driver) {
 		
- 		//"initElements" revisa e inicializa los elementos (los WebElements).
- 		//el parametro "this", hace que los elementos inicien en "esta" clase en el navegador.
 		PageFactory.initElements(driver, this);
 	}
-	//Metodo que representa la accion sobre LinkSign (hipervin), solo se puede hacer click().
+
 	public void clickedSignIn() {
-		linkSignIn.click();           //"Accion" de hacer click in Sign In.
+		linkSignIn.click();
 	}
 	
 	
-	public void emailCreateAcc(String emailAcc) { //email create an account.
+	public void emailCreateAcc(String emailAcc) {
 		emailCreateAccount.sendKeys(emailAcc);
 	}
 	
-	public void clickBtnSumitAcc() {  //click buttom create an account.
+	public void clickBtnSumitAcc() {
 		btnSubmitCreate.click();
 	}
 
-	public void searchWord (String word) {  //Representa la palabra a buscar y se la identifica
-		txtSearch.sendKeys(word);           // con una variable String como parametro
+	public void searchWord (String word) {
+		txtSearch.sendKeys(word);
 	}
 	
 	public void clickedInLens () {
